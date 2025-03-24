0291d0210c048bfeb92aa6ed4f12df03791d052f
SeoBamm <70587328+SeoBamm@users.noreply.github.com>
2025-03-24 22:18:15 +0900

    Initialize
    .

---
+++ InstanTrip/src/main/java/com/instantrip/instantrip_backend/InstantripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.instantrip.instantrip_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantripBackendApplication.class, args);
+	}
+
+}
---
+++ InstanTrip/src/test/java/com/instantrip/instantrip_backend/InstantripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instantrip.instantrip_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
